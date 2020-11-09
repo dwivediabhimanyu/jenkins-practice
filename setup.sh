a43eb6bedaacd71d6cbe49509d2928a97807e2bb
dvlprabhi <hey@dvlprabhi.me>
2020-11-10 00:34:35 +0530

    mern demo

---
+++ setup.sh
@@ -0,0 +1,7 @@
+#!/bin/bash
+npm install pm2 -g
+npm install
+cd client 
+npm install
+npm run build
+cd ../
---
+++ start.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+export NODE_ENV=production
+pm2 start ./start.sh --name myApp
